f97c8ad5bd110ef90b7c9049a8ff7289f9cbe17f
RahulNeupane <nrahul503@gmail.com>
2022-07-31 21:24:48 +0545

    scroll animation

--- script.js
+++ script.js
@@ -1,26 +1,8 @@
-// const boxes = document.querySelectorAll('.box');
-
-// window.addEventListener('scroll',checkBox)
-
-// function checkBox(){
-//   const trigger =   window.innerHeight / 5 * 3
-
-//   boxes.forEach(box=>{
-//     const boxTop = box.getBoundingClientRect().top;
-
-//     if(boxTop<trigger){
-//         box.classList.add('show');
-//     }
-//     else{
-//         box.classList.remove('show')
-//     }
-
-//   })
-// }
-
-
-
 const boxes = document.querySelectorAll('.box')
+boxes.forEach(box=>{
+    fetch('https://dog.ceo/api/breeds/image/random').then(response=>response.json()).then(data => box.style.background = `url('${data.message}') no-repeat center center/cover`)
+})
+
 window.addEventListener('scroll',showBox)
 
 function showBox(){
